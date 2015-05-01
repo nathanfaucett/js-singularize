435ebda1730d65032d20954ce1684b84572f2359
Nathan Faucett <nathanfaucett@gmail.com>
2015-05-01 12:25:48 -0500

    updates

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 var inflections = require("inflections");
 
 
-module.exports = function singularize(string, locale) {
+module.exports = singularize;
+
+
+function singularize(string, locale) {
     return inflections(locale).singularize(string);
-};
+}
